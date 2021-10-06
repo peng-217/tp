d9b1f0ec39d3735537a4a517cd305ca74e0f0c07
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-10-07 00:44:49 +0800

    Add utility function for suspect list

--- src/main/java/suspect/SuspectList.java
+++ src/main/java/suspect/SuspectList.java
@@ -1,7 +1,10 @@
 package suspect;
 
+import clue.Clue;
 import seedu.duke.Ui;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SuspectList {
@@ -19,6 +22,22 @@ public class SuspectList {
 
     public void addSuspect(String name, Suspect suspect) {
         suspects.put(name, suspect);
+    }
+
+    public void addClueForSuspect(String name, Clue clue) {
+        suspects.get(name).addClue(clue);
+    }
+
+    public void setClueChecked(String name, Clue clue) {
+        suspects.get(name).setChecked(clue);
+    }
+
+    public ArrayList<Clue> getSuspectAllClues(String name) {
+        return suspects.get(name).getClues();
+    }
+
+    public ArrayList<Clue> getSuspectAvailableClues(String name) {
+        return suspects.get(name).getAvailableClues();
     }
 
     public int getNumSuspects() {
--- src/test/java/suspect/SuspectListTest.java
+++ src/test/java/suspect/SuspectListTest.java
@@ -1,7 +1,13 @@
 package suspect;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import clue.Clue;
+import clue.FatherInsurance;
+import clue.FatherMap;
+import clue.FatherTextMessage;
 import org.junit.jupiter.api.Test;
 import seedu.duke.Ui;
 
@@ -15,5 +21,31 @@ public class SuspectListTest {
         suspects.addSuspect("Tom", new Suspect());
 
         assertEquals("[Tom, Jerry]", suspects.getSuspects().keySet().toString());
+    }
+
+    @Test
+    public void listOfSuspectsWithClues() {
+        Ui ui = new Ui();
+        SuspectList suspects = new SuspectList(ui);
+        suspects.addSuspect("Jerry", new Suspect());
+        suspects.addSuspect("Tom", new Suspect());
+
+        Clue fatherInsurance = new FatherInsurance();
+        Clue fatherTextMessage = new FatherTextMessage();
+        Clue fatherMap = new FatherMap();
+
+        suspects.addClueForSuspect("Tom", fatherInsurance);
+        suspects.addClueForSuspect("Tom", fatherTextMessage);
+        suspects.addClueForSuspect("Jerry", fatherMap);
+
+        assertEquals(2, suspects.getSuspectAvailableClues("Tom").size());
+
+        assertFalse(suspects.getSuspectAllClues("Tom").get(0).isChecked());
+        suspects.setClueChecked("Tom", fatherInsurance);
+        assertTrue(suspects.getSuspectAllClues("Tom").get(0).isChecked());
+
+        assertEquals(1, suspects.getSuspectAvailableClues("Tom").size());
+
+//        System.out.println(suspects.getSuspects().get("Jerry").getClues().get(0).isChecked());
     }
 }
