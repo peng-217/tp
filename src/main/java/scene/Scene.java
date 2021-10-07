2957fe8767972b4d8b4df0effd2c39eb29fb66e2
Herrekt <herrickkohyukan@gmail.com>
2021-10-07 16:50:06 +0800

    Updated Scene class and added tests relating to it.

--- src/main/java/scene/Scene.java
+++ src/main/java/scene/Scene.java
@@ -1,22 +1,35 @@
 package scene;
 
 import narrative.Narrative;
-import search.Search;
+import suspect.SuspectList;
+
+import java.io.FileNotFoundException;
 
 public class Scene {
     private final Narrative narrative;
-    private final Search clueSearch;
+    private final SuspectList suspects;
 
-    public Scene(Narrative narrative, Search clueSearch) {
+    public Scene(Narrative narrative, SuspectList suspects) {
         this.narrative = narrative;
-        this.clueSearch = clueSearch;
+        this.suspects = suspects;
+
     }
 
     public Narrative getNarrative() {
         return narrative;
     }
 
-    public Search getClueSearch() {
-        return clueSearch;
+    public void runScene() throws FileNotFoundException {
+        this.narrative.displayNarrative();
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return this.narrative.getNarrative() + "\n" + this.suspects.getSuspects().keySet().toString();
+        } catch (FileNotFoundException e) {
+            System.out.println("Narrative has not been selected!");
+            return "Incomplete Scene";
+        }
     }
 }
