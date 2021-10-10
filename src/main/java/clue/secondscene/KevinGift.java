94a90844af4a92ea7c9748e4b56787b2a89e7c9f
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-10-11 04:20:45 +0800

    Update KevinGift clue

--- src/main/java/clue/secondscene/KevinGift.java
+++ src/main/java/clue/secondscene/KevinGift.java
@@ -6,9 +6,15 @@ public class KevinGift extends Clue {
 
     public KevinGift() {
         super();
-        this.clueName = "              Gift";
-        this.image = "";
-        this.description = "The gift Kevin brought was a very expensive Naruto"
-                + "garage kit. I've been eyeing it for a long time.";
+        this.clueName = "                     Gift";
+        this.image = "             _________________\n"
+                + "            |'-========OoO===='-.\n"
+                + "            | ||'-.____'-.'-.____'-.\n"
+                + "            | ||  |      |  |      |\n"
+                + "             '-|  |      |  |      |\n"
+                + "                '-|______|__|______|";
+        this.description = "The gift Kevin brought was a very expensive\n"
+                + "Naruto figurine. I've been wanting it for a\n"
+                + "long time.";
     }
 }
