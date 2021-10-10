46487c69665a57ca3838a7f9ac2f092147617e40
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-10-10 21:42:34 +0800

    Implement scene list class for multiple scenes

---
+++ src/test/java/scene/SceneListTest.java
@@ -0,0 +1,49 @@
+package scene;
+
+import clue.Clue;
+import clue.FatherInsurance;
+import clue.FatherPhoneCall;
+import clue.FatherTextMessage;
+import narrative.FirstNarrative;
+import narrative.SecondNarrative;
+import org.junit.jupiter.api.Test;
+import search.Search;
+import seedu.duke.Ui;
+import suspect.Suspect;
+import suspect.SuspectList;
+
+import java.io.FileNotFoundException;
+
+public class SceneListTest {
+
+    @Test
+    public void scenesTest() throws FileNotFoundException {
+        Ui ui = new Ui();
+        SuspectList suspects = new SuspectList(ui);
+        suspects.addSuspect("Jerry", new Suspect());
+        suspects.addSuspect("Tom", new Suspect());
+
+        Clue fatherInsurance = new FatherInsurance();
+        Clue fatherMap = new FatherTextMessage();
+        suspects.addClueForSuspect("Tom", fatherInsurance);
+        suspects.addClueForSuspect("Tom", fatherMap);
+
+        Clue fatherPhoneCall = new FatherPhoneCall();
+        suspects.addClueForSuspect("Jerry", fatherPhoneCall);
+
+        Search searchFirstScene = new Search(suspects);
+        Scene firstScene = new Scene(new FirstNarrative(), searchFirstScene);
+
+        Search searchSecondScene = new Search(suspects);
+        Scene secondScene = new Scene(new SecondNarrative(), searchSecondScene);
+
+        SceneList sceneList = new SceneList();
+        sceneList.addScene(firstScene);
+        sceneList.addScene(secondScene);
+
+        sceneList.getCurrentScene().runScene();
+        sceneList.nextScene();
+        System.out.println("------------------------------------------");
+        sceneList.getCurrentScene().runScene();
+    }
+}
