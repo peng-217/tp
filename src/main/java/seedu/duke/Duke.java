90066bd5137172100de4cb22ee278c9203f904bc
MaifengNg <maifeng26@hotmail.com>
2021-10-10 17:13:45 +0800

    Add Ui, Parser class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,47 +2,59 @@ package seedu.duke;
 
 import suspect.Suspect;
 import suspect.SuspectList;
-
 import java.util.Scanner;
+
+import Ui.Ui;
+import Parser.Parser;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     private static Ui ui;
+    private static Parser parser;
     private static Suspect choseSuspect;
     private static Suspect realSuspect;
 
-    public static void initializeGame() {
-        ui = new Ui();
-        SuspectList suspects = new SuspectList(ui);
+    private static String userName;
 
+    public static void initializeGame() {
+        // Initialise new parser object
+        parser = new Parser();
+
+        // Initialise a new Ui object
+        ui = new Ui();
+        ui.printIntroductionMessage();
+
+        // We ask the user to give a name
+        ui.askForUsername();
+        ui.printEmptyLine();
+        userName = ui.readUserInput();
+        ui.printEmptyLine();
+
+        // print welcome message with username
+        ui.printEmptyLine();
+        ui.printWelcomeMessage(userName);
+        ui.printEmptyLine();
+//        SuspectList suspects = new SuspectList(ui);
 
     }
 
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
-        String userInput;
-        Scanner userType = new Scanner(System.in);
-        userInput = userType.nextLine();
-
-        while (!userInput.equals("EXIT")) {
-            //parser.processCommand(userInput);
-            //the line above needs parser to be done first, then can be uncommented
-            userInput = in.nextLine();
+        initializeGame();
+        boolean userExit = false;
+        while (!userExit) {
+            String userInput = ui.readUserInput();
+            String userInputParsed = parser.parseUserInput(userInput);
+            ui.printEmptyLine();
+            if (userInputParsed.equals("/exit")) {
+                userExit = true;
+            } else if (userInputParsed.equals("/help")) {
+                ui.printListOfCommands();
+            }
+            ui.printEmptyLine();
         }
-
-
         ui.printExitMessage();
     }
 }
