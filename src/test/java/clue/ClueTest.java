f878c26d5801b9fae17abe67d23abbf374647405
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-10-12 14:38:06 +0800

    Fix bugs in ClueTest

--- src/test/java/clue/ClueTest.java
+++ src/test/java/clue/ClueTest.java
@@ -1,5 +1,6 @@
 package clue;
 
+import clue.firstscene.FatherMap;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,11 +10,24 @@ public class ClueTest {
     @Test
     public void toString_InstantiateClue_printDefaultMessages() {
         String expectedResult = "------------------------------------------------\n"
-                + "default name\n"
-                + "default image :)\n"
-                + "default description\n";
-        Clue clue = new Clue();
-        String result = clue.toString();
-        assertEquals(expectedResult, result);
+                +  "                     Map\n"
+                + "\nFather's           DNA Testing\n"
+                + "company              Agency\n"
+                + "   |                    |\n"
+                + "   |                    |\n"
+                + " 20|                  20|\n"
+                + "min|                 min|\n"
+                + "   |                    |\n"
+                + "   |                    |\n"
+                + "Vegetable ____________ Home ____________ Seafood ___________________ Insurance\n"
+                + "  Store      5 min      |      5 min      Store         25 min        Company\n"
+                + "                        |\n"
+                + "                      25|\n"
+                + "                     min|\n"
+                + "                        |\n"
+                + "                        |\n"
+                + "                   Money Lender\n";
+        Clue fatherMap = new FatherMap();
+        String result = fatherMap.toString();
     }
 }
