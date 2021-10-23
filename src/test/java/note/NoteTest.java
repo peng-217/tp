5c071ff0194dfcfe8b25cfb7b775401baf028329
peng-217 <pengfei20020217@163.com>
2021-10-23 20:07:58 +0800

    Add test code for Note and NoteList class

---
+++ src/test/java/note/NoteTest.java
@@ -0,0 +1,27 @@
+package note;
+
+import org.junit.jupiter.api.Test;
+import ui.Ui;
+import note.NoteList;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteTest {
+
+    @Test
+    public void getNoteContent() {
+        Note testingNoteContent = new Note("Test note","DEFAULT",1);
+        assertEquals("Test note",testingNoteContent.getNoteContent());
+    }
+
+    @Test
+    public void getNoteSceneIndex() {
+        Note testingNoteIndex = new Note("Test note","DEFAULT",2);
+        assertEquals(2,testingNoteIndex.getNoteSceneIndex());
+    }
+
+    @Test
+    public void getNoteTitle() {
+        Note testingNoteTitle = new Note("Test note", "TEST TITLE", 3);
+        assertEquals("TEST TITLE", testingNoteTitle.getNoteTitle());
+    }
+}
