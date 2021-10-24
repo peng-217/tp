e4d29b7b75bb73cfed8b5008ac40d474bcaac4a7
MaifengNg <maifeng26@hotmail.com>
2021-10-25 00:31:15 +0800

    Add fix checkstyle

--- src/test/java/exceptions/ExceptionTest.java
+++ src/test/java/exceptions/ExceptionTest.java
@@ -14,13 +14,14 @@ import suspect.SuspectList;
 import ui.Ui;
 
 public class ExceptionTest {
+    private static final Ui ui = new Ui();
+    private static final String GAME_DATA_FILE_NAME = "GameData.txt";
+    private static final GameDataFileDecoder dataFile =
+            new GameDataFileDecoder(ui, new GameDataFileManager(GAME_DATA_FILE_NAME));
 
     @Test
     public void throwInvalidSuspectExceptionTest() {
-        Ui ui = new Ui();
-        String GAME_DATA_FILE_NAME = "GameData.txt";
-        GameDataFileDecoder dataFile =
-                new GameDataFileDecoder(ui, new GameDataFileManager(GAME_DATA_FILE_NAME));
+
         try {
             SceneList sceneList = SceneListBuilder.buildSceneList(ui, dataFile);
             SuspectList clueTracker = CheckedClueTrackerBuilder.buildClueTracker();
@@ -34,21 +35,21 @@ public class ExceptionTest {
 
     }
 
-//    @Test
-//    public void throwInvalidClueExceptionTest() {
-//        Ui ui = new Ui();
-//        String GAME_DATA_FILE_NAME = "GameData.txt";
-//        GameDataFileDecoder dataFile =
-//                new GameDataFileDecoder(ui, new GameDataFileManager(GAME_DATA_FILE_NAME));
-//        try {
-//            SceneList sceneList = SceneListBuilder.buildSceneList(ui, dataFile);
-//            SuspectList clueTracker = CheckedClueTrackerBuilder.buildClueTracker();
-//
-//            Investigation investigation = new Investigation(sceneList, clueTracker);
-//            investigation.restartGame();
-//            assertThrows(InvalidSuspectException.class, () -> investigation.investigateScene(10));
-//        } catch (MissingSceneFileException e) {
-//            ui.printMissingSceneFileMessage();
-//        }
-//    }
+    //@Test
+    //public void throwInvalidClueExceptionTest() {
+    //    Ui ui = new Ui();
+    //    String GAME_DATA_FILE_NAME = "GameData.txt";
+    //    GameDataFileDecoder dataFile =
+    //            new GameDataFileDecoder(ui, new GameDataFileManager(GAME_DATA_FILE_NAME));
+    //    try {
+    //        SceneList sceneList = SceneListBuilder.buildSceneList(ui, dataFile);
+    //        SuspectList clueTracker = CheckedClueTrackerBuilder.buildClueTracker();
+    //
+    //        Investigation investigation = new Investigation(sceneList, clueTracker);
+    //        investigation.restartGame();
+    //        assertThrows(InvalidSuspectException.class, () -> investigation.investigateScene(10));
+    //    } catch (MissingSceneFileException e) {
+    //        ui.printMissingSceneFileMessage();
+    //    }
+    //}
 }
