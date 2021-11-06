00c0e5df5381078d2531808dd46643d2a33595bd
ShaoYurui <rrayshao@gmail.com>
2021-11-06 15:33:06 +0800

    Update DG

--- src/main/java/storage/GameFileManager.java
+++ src/main/java/storage/GameFileManager.java
@@ -5,8 +5,8 @@ import exception.DukeFileNotFoundException;
 
 public class GameFileManager extends EncryptedFile {
 
-    static FileDecoder decoder;
-    static FileEncoder encoder;
+    private static FileDecoder decoder;
+    private static FileEncoder encoder;
 
     public GameFileManager(String fileName) throws DukeFileNotFoundException, DukeCorruptedFileException {
         super(fileName);
