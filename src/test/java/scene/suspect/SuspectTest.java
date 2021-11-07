9cac85ec3ee29f81ad71c4c7a5f0daee066d784e
arcturusz <48401155+arcturusz@users.noreply.github.com>
2021-11-07 15:59:52 +0800

    Add more test for scene and suspect

--- src/test/java/scene/suspect/SuspectTest.java
+++ src/test/java/scene/suspect/SuspectTest.java
@@ -7,15 +7,27 @@ import scene.clue.firstscene.FatherInsurance;
 import scene.clue.firstscene.FatherMap;
 import scene.clue.firstscene.FatherTextMessage;
 import org.junit.jupiter.api.Test;
+import scene.clue.secondscene.FatherDiary;
+import scene.clue.secondscene.KevinBro;
+
+import java.util.ArrayList;
 
 public class SuspectTest {
 
     @Test
     public void suspectWithTwoClues() {
         Suspect tom = new Suspect();
-        tom.addClue(new FatherInsurance());
-        tom.addClue(new FatherTextMessage());
-        assertEquals(2, tom.getClues().size());
+        Clue fatherInsurance = new FatherInsurance();
+        Clue fatherTextMessage = new FatherTextMessage();
+
+        tom.addClue(fatherInsurance);
+        tom.addClue(fatherTextMessage);
+        assertEquals(2, tom.getNumClues());
+
+        ArrayList<Clue> allClues = new ArrayList<>();
+        allClues.add(fatherInsurance);
+        allClues.add(fatherTextMessage);
+        assertEquals(allClues, tom.getClues());
     }
 
     @Test
@@ -36,6 +48,32 @@ public class SuspectTest {
 
         tom.setChecked(fatherMap);
         assertEquals(0, tom.getAvailableClues().size());
+    }
+
+    @Test
+    public void markClue_ClueNotFound() {
+        Suspect father = new Suspect();
+        Clue fatherInsurance = new FatherInsurance();
+        father.addClue(fatherInsurance);
+
+        Clue kevinBro = new KevinBro();
+        father.setChecked(kevinBro);
+    }
+
+    @Test
+    public void getCheckedCluesTest_TwoClues() {
+        Suspect father = new Suspect();
+        Clue fatherInsurance = new FatherInsurance();
+        Clue fatherDiary = new FatherDiary();
+
+        father.addClue(fatherInsurance);
+        father.addClue(fatherDiary);
+
+        father.setChecked(fatherDiary);
+
+        ArrayList<Clue> correctClues = new ArrayList<>();
+        correctClues.add(fatherDiary);
+        assertEquals(correctClues, father.getCheckedClues());
     }
 
 }
